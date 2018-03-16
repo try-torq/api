6eee433d1a2c0cedbc6c9f2c69275cc76d7988d0
Charles Kenney <Charlesc.kenney@gmail.com>
2018-03-16 04:05:42 -0400

    fix field case p1

--- src/schemas/fields/FirstNameField.ts
+++
@@ -1,8 +0,0 @@
-import { GraphQLString, GraphQLFieldConfig, GraphQLField } from 'graphql';
-
-export class FirstnameField implements GraphQLField<any, any> {
-  public type = GraphQLString;
-  public name = 'firstname';
-  public description = 'The first name of the user.';
-  public args;
-}
--- src/schemas/fields/LastNameField.ts
+++
@@ -1,8 +0,0 @@
-import { GraphQLString, GraphQLFieldConfig, GraphQLField } from 'graphql';
-
-export class LastnameField implements GraphQLField<any, any> {
-  public type = GraphQLString;
-  public name = 'lastname';
-  public description = 'The last name of the user.';
-  public args;
-}
